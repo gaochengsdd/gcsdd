67ad89e25bbf7849db2383e159107e7031eb0f77
高成 <1437549947@qq.com>
2019-12-20 15:59:15 +0800

    test

---
+++ src/My.php
@@ -0,0 +1,27 @@
+<?php
+
+    namespace Gaocheng;
+
+    class My{
+
+        private $msg = "welcome";
+
+        public function sendmsg(){
+            echo $this->msg;
+        }
+
+        public function message($msg=null){
+            if($msg){
+                echo "没有输入";
+            }else{
+                echo "输入的是：".$msg;
+            }
+        }
+        
+        public function index(){
+            
+        }
+
+    }
+
+?>
